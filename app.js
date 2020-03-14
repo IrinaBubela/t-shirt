f95967c786f9ed1e5657562bcca2ce1ab9f8ffd4
Irina <rishabubela@gmail.com>
2020-03-15 00:28:10 +0100

    Finished a structure

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 
+const img1Carousel = document.getElementById('img1Carousel');
 
 
-// $('#img1').click(function () {
-//     $("#img1Carousel").attr('src', 'img/fourth.jpg');
-// });
+$('#img1, #img2, #img3').click(function () {
+    toggleImage($(this).attr('id'))
+})
 
-// $('#img2').click(function () {
-//     $("#img2Carousel").attr('src', 'img/second.jpg');
-// });
-
-$('#img1').click(function () {
-    var $img1Carousel = $('#img1Carousel');
-    if ($img1Carousel.attr('src', 'img/second.jpg')) {
-        $img1Carousel.removeAttr('src', 'img/second.jpg');
-    } else {
-        $img1Carousel.attr('src', 'img/second.jpg');
+function toggleImage(imageId) {
+    console.log(imageId);
+    if (imageId === "img1") {
+        $(".image").attr('src', 'img/first.jpg');
+        $("slideEngine").toggleClass("active")
     }
-});
+    if (imageId === "img2") {
+        $(".image").attr('src', 'img/second.jpg');
+        $("slideEngine").toggleClass("active")
+    }
+    if (imageId === "img3") {
+        $(".image").attr('src', 'img/third.jpg');
+        $("slideEngine").toggleClass("active")
+    }
+};
+
